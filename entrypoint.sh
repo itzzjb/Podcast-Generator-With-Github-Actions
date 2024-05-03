329d0c86fd608e93d4c2ab817e5a7a3f281889f9
Januda Bethmin <desilvabethmin@gmail.com>
2024-05-03 18:10:17 +0000

    created entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+# This will be called from the container which is running the docker image
+# Used to write scripts to command-line
+
+#!/bin/bash
+echo "==========================="
+
+# Getting the name and email of the user 
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+
+# Adding to the safelist of directories
+git config --global --add safe.derectory /github/workspace
+
+python3 /usr/bin/feed.py
+
+git add .
+git commit -m "Update feed"
+git push --set-upstream origin main
+
+echo "==========================="
+
+
+
+
+
